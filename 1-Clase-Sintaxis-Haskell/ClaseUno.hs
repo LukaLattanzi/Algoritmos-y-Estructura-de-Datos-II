10d17566a5e0d31ea98b620061a4b841f7b74052
Luka Lattanzi <lattanziluka203@gmail.com>
2025-04-07 18:05:15 -0300

    Commit 1 7/4/2025

---
+++ 1-Clase-Sintaxis-Haskell/ClaseUno.hs
@@ -0,0 +1,151 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use guards" #-}
+{-# HLINT ignore "Use foldr" #-}
+
+add' :: (Int, Int) -> Int
+add' (x, y) = x + y
+
+zeroto :: Int -> [Int]
+zeroto x = [0 .. x]
+
+-- Una funcion es polimorfica si su tipo contiene una o mas variables de tipo.
+
+length' :: [a] -> Int
+length' [] = 0
+length' (_ : xs) = 1 + length' xs
+
+-- Las funciones pueden ser definidas usando expresiones condicionales.
+
+abs :: Int -> Int
+abs n = if n > 0 then n else -n
+
+-- Las expresiones condicionales pueden estar anidadas:
+
+signum :: Int -> Int
+signum n =
+  if n < 0
+    then -1
+    else
+      if n == 0 then 0 else 1
+
+-- Una alternativa a los condicionales es el uso de ecuaciones con guardas.
+
+abs' :: (Ord a, Num a) => a -> a
+abs' n
+  | n > 0 = n
+  | otherwise = -n
+
+-- Toda lista no vacia se construye usando el operador (:) llamado "cons" que agrega un elemento al principio de la lista. [1,2,3,4] resulta de 1:(2:(3:(4:[])))
+
+-- Las funciones pueden construirse sin nombres usando expresiones lambda y en el teclado se ingresa \.
+
+odds' :: (Num b, Enum b) => b -> [b]
+odds' n = map f [0 .. n - 1]
+  where
+    f x = x * 2 + 1
+
+odds'' :: (Num b, Enum b) => b -> [b]
+odds'' n = map (\x -> x * 2 + 1) [0 .. n - 1]
+
+-- Una manera de contruir conjuntos a partir de conjuntos existentes es con la notacion por comprension.
+
+concat :: [[a]] -> [a]
+concat xss = [x | xs <- xss, x <- xs]
+
+-- > concat [[1,2,3],[4,5],[6]] => [1,2,3,4,5,6]
+
+factors :: Int -> [Int]
+factors n = [x | x <- [1 .. n], n `mod` x == 0]
+
+-- Como un numero n es primo si y solo si sus unicos factores son 1 y n podemos definir:
+
+prime :: Int -> Bool
+prime n = factors n == [1, n]
+
+primes :: Int -> [Int]
+primes n = [x | x <- [2 .. n], prime x]
+
+-- La funcion zip, mapea dos listas a una lista con los pares de elementos correspondientes. zip :: [a] -> [b] -> [(a,b)]. Usando zip podemos definir una funcion que retorna la lista de pares de elementos adyacentes.
+
+pairs :: [a] -> [(a, a)]
+pairs xs = zip' xs (tail xs)
+
+-- pairs [1, 2, 3, 4] == [(1, 2), (2, 3), (3, 4)]
+
+-- Podemos usar pairs para decicir si una lista esta ordenada.
+
+sorted :: (Ord a) => [a] -> Bool
+sorted xs = and [x <= y | (x, y) <- pairs xs]
+
+-- Podemos usar zip para generar una funcion que retorne la listas de posiciones de un valor determinado
+
+positions :: (Num a1, Enum a1, Eq a2) => a2 -> [a2] -> [a1]
+positions x xs = [i | (x', i) <- zip' xs [0 ..], x == x']
+
+-- > positions 0 [1,0,0,1,0,1,1,0] => [1,2,4,7]
+
+-- Una String es una lista de caracteres [Char]. Todas las funciones sobre listas son aplicables a String.
+
+count :: (Eq a) => a -> [a] -> Int
+count x xs = length [x' | x' <- xs, x == x']
+
+-- count 's' "Mississippi" => 4
+
+-- Como hemos visto las funciones pueden definirse en terminos de otras funciones.
+
+factorial :: (Num a, Enum a) => a -> a
+factorial n = product [1 .. n]
+
+-- En haskell las funciones pueden ser definidas en terminos de ellas mismas (recursion).
+
+factorial' :: (Eq a, Num a, Enum a) => a -> a
+factorial' 0 = 1
+factorial' n = n * factorial (n - 1)
+
+{-
+factorial 3
+= 3 * factorial 2
+= 3 * (2 * factorial 1)
+= 3 * (2 * (1 * factorial 0))
+= 3 * (2 * (1 * 1))
+= 6
+-}
+product' :: (Num a) => [a] -> a
+product' [] = 1
+product' (n : ns) = n * product' ns
+
+-- Usando la recursion podemos definir el reverso de una lista.
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x : xs) = reverse' xs ++ [x]
+
+{-
+reverse [1,2,3]
+= reverse [2,3] ++ [1]
+= (reverse [3] ++ [2]) ++ [1]
+= ((reverse [ ] ++ [3]) ++ [2]) ++ [1]
+= (([ ] ++ [3]) ++ [2]) ++ [1]
+= [3,2,1]
+-}
+
+zip :: [a] -> [b] -> [(a, b)]
+
+zip' [] _ = []
+zip' _ [] = []
+
+zip (x : xs) (y : ys) = (x, y) : zip' xs ys
+
+{-
+El algoritmo de ordenacion quicksort:
+  La lista vacia esta ordenada.
+  La listas no vacias pueden ser ordenadas, ordenando los valores de la cola <= que la cabeza, ordenando los valores > que la cabeza y rearmando el resultado con las listas resultantes a ambos lados de la cabeza.
+-}
+
+qsort :: (Ord a) => [a] -> [a]
+qsort [] = []
+qsort (x : xs) = qsort chicos ++ [x] ++ qsort grandes
+  where
+    chicos = [a | a <- xs, a <= x]
+    grandes = [b | b <- xs, b > x]
---
+++ 2. Clase Tipos Haskell/ClaseDos.hs
@@ -0,0 +1,169 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use foldr" #-}
+import Prelude hiding (Bool (..), Maybe (..), String)
+
+-- En haskell podemos definir un nuevo nombre para un tipo existente usando una declaracion type.
+
+type String = [Char]
+
+-- Los sinonimos de tipo hace que ciertas declaraciones de tipos sean mas faciles de leer.
+
+type Pos = (Int, Int)
+
+origin :: Pos
+origin = (0, 0)
+
+left :: Pos -> Pos
+left (x, y) = (x - 1, y)
+
+-- Como las funciones, la declaracion de tipos puede tener parametros:
+
+type Par a = (a, a)
+
+mult :: Par Int -> Int
+mult (x, y) = x * y
+
+copy :: a -> Par a
+copy x = (x, x)
+
+-- Las declaraciones de tipo pueden anidarse, Type Pos = (Int, Int). Pero no pueden ser recursivos type Tree = (Int,[Tree]).
+
+-- Un nuevo tipo completo puede ser definido dando sus valores con la declaracion data.
+
+data Bool = False | True deriving (Eq, Show)
+
+-- True y False son los llamdos constructores del tipo Bool, estos deben empezar con mayusculas.
+
+data Answer = Yes | No | Unknown deriving (Eq, Show)
+
+flip :: Answer -> Answer
+flip Yes = No
+flip No = Yes
+flip Unknown = Unknown
+
+-- Los constructores en las declaraciones data pueden tener parametros.
+
+data Shape = Circle Float | Rect Float Float deriving (Eq, Show)
+
+square :: Float -> Shape
+square n = Rect n n
+
+area :: Shape -> Float
+area (Circle r) = 3.14 * (r * r)
+area (Rect x y) = x * y
+
+-- No es sorpresa que las declaraciones data pueden tambien tener parametros de tipos.
+
+data Maybe a = Nothing | Just a deriving (Eq, Show)
+
+safediv :: Int -> Int -> Maybe Int
+safediv _ 0 = Nothing
+safediv m n = Just (m `div` n)
+
+safehead :: [a] -> Maybe a
+safehead [] = Nothing
+safehead xs = Just (head xs)
+
+-- Las declaraciones data pueden ser recursivas.
+
+data Nat = Zero | Succ Nat deriving (Eq, Show)
+
+add :: Nat -> Nat -> Nat
+add n Zero = n
+add n (Succ m) = Succ (add m n)
+
+-- Nat es un nuevo tipo con constructores Zero :: Nat y Succ :: Nat -> Nat.
+-- Un valor de tipo Nat puede ser Zero o de la forma Succ n donde n :: Nat.
+{-
+Podemos pensar a los valores del tipo Nat como numeros naturales donde:
+    Zero representa a 0 y Succ representa la funcion sucesor (1+)
+    Succ (Succ (Succ Zero)) = (1 + 1 + 1 + 0) = 3
+-}
+
+-- Usando recursion es facil definir funciones que conviertan los valores entre los tipos Nat e Int.
+
+nat2int :: Nat -> Int
+nat2int Zero = 0
+nat2int (Succ n) = 1 + nat2int n
+
+int2nat :: Int -> Nat
+int2nat 0 = Zero
+int2nat n = Succ (int2nat (n - 1))
+
+-- Dos naturales pueden ser sumados convirtiendolos en enteros, sumarlos, y convertilos nuevamente a naturales.
+
+add' :: Nat -> Nat -> Nat
+add' m n = int2nat (nat2int m + nat2int n)
+
+-- Sin embargo usando recursion la misma funcion se puede definir sin la necesidad de conversion.
+
+add'' :: Nat -> Nat -> Nat
+add'' Zero n = n
+add'' (Succ m) n = Succ (add'' m n)
+
+-- Ejercicio definir la multiplicacion para Nat.
+
+mulnat :: Nat -> Nat -> Nat
+mulnat m n = int2nat (nat2int m * nat2int n)
+
+mulnat' :: Nat -> Nat -> Nat
+mulnat' _ Zero = Zero
+mulnat' m (Succ n) = add' m (mulnat' m n)
+
+-- Ejercicio definir la exponenciacion para Nat.
+
+expnat :: Nat -> Nat -> Nat
+expnat m n = int2nat (nat2int m ^ nat2int n)
+
+expnat' :: Nat -> Nat -> Nat
+expnat' _ Zero = Succ Zero
+expnat' m (Succ n) = mulnat m (expnat m n)
+
+-- Las declaraciones data pueden ser recursivas y con parametros.
+
+data List a = Nil | Cons a (List a) deriving (Eq, Show)
+
+-- Usando recursion es facil definir funciones que conviertan los valores entre los tipos List a y [a].
+
+tolist :: List a -> [a]
+tolist Nil = []
+tolist (Cons x xs) = x : tolist xs
+
+fromlist :: [a] -> List a
+fromlist [] = Nil
+fromlist (x : xs) = Cons x (fromlist xs)
+
+-- Sintaxis de registro (Record), pdemos crear un tipo que describa a una persona.
+
+data Person = Person
+  { firstName :: String,
+    lastName :: String,
+    age :: Int,
+    height :: Float,
+    phoneNumber :: String,
+    flavor :: String
+  }
+  deriving (Show)
+
+examplePerson :: Person
+examplePerson =
+  Person
+    { lastName = "Finklestein",
+      firstName = "Buddy",
+      height = 184.2,
+      age = 43,
+      flavor = "chocolate",
+      phoneNumber = "526-2928"
+    }
+
+-- Nota que no es necesario respetar el orden en que fueron declarados los campos.
+
+-- Ademas de pattern matching en el lado izq. de una definicion, podemos usar un expresion case.
+
+esCero :: Nat -> Bool
+esCero n = case n of
+  Zero -> True
+  _ -> False
+
+-- Los patrones de los diferentes cosas son intentados en orden, se usa la identacion para marcar un bloque de casos.
